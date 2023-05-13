e56d2994afbb56ce3a0d6dcb0fa6311df6d25ee0
Dominic Goj <dominicgoj@gmail.com>
2023-05-13 14:45:27 +0200

    removed stored

--- foodupdjango/foodupdjango/urls.py
+++ foodupdjango/foodupdjango/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('stores.urls')),
     path('', include('ratings.urls')),
 ]
 
