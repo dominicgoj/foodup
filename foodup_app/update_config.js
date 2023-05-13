de3d1dc15e8fa7d4ea402c0458a2e673201c4bb9
Dominic Goj <dominicgoj@gmail.com>
2023-05-13 14:27:33 +0200

    new version

--- foodup_app/update_config.js
+++ foodup_app/update_config.js
@@ -1,31 +1,24 @@
 const fs = require('fs');
 const axios = require('axios');
 
-// Fetch the public IP address
-axios.get('https://api.ipify.org/?format=json')
-  .then(response => {
+const updateConfig = async () => {
+  try {
+    // Fetch the public IP address
+    const response = await axios.get('https://api.ipify.org/?format=json');
     const publicIp = response.data.ip;
 
     // Read the config.js file
-    fs.readFile('./config.js', 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
+    const data = await fs.promises.readFile('./config.js', 'utf8');
 
-      // Update the backend URL in the config.js file
-      const updatedData = data.replace(/http:\/\/\d+\.\d+\.\d+\.\d+:\d+/, `http://${publicIp}:8000`);
+    // Update the backend URL in the config.js file
+    const updatedData = data.replace(/http:\/\/\d+\.\d+\.\d+\.\d+:\d+/, `http://${publicIp}:8000`);
 
-      // Write the updated config.js file
-      fs.writeFile('./config.js', updatedData, 'utf8', err => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        console.log('Config file updated successfully.');
-      });
-    });
-  })
-  .catch(error => {
+    // Write the updated config.js file
+    await fs.promises.writeFile('./config.js', updatedData, 'utf8');
+    console.log('Config file updated successfully.');
+  } catch (error) {
     console.error('Failed to fetch public IP:', error.message);
-  });
+  }
+};
+
+updateConfig();
