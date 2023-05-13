2c28a010e6216cf28c2e6556ad30d4c6ad6aa881
Dominic Goj <dominicgoj@gmail.com>
2023-05-13 10:22:47 +0200

    patched averagerating.py to set rating to zero if no posts are available

--- foodupdjango/ratings/management/commands/averagerating.py
+++ foodupdjango/ratings/management/commands/averagerating.py
@@ -1,5 +1,3 @@
-# calculate_average_rating.py
-
 from django.core.management.base import BaseCommand
 from ratings.models import Restaurant, Post
 from django.db.models import Avg
@@ -15,10 +13,10 @@ class Command(BaseCommand):
         for restaurant in restaurants:
             # Calculate average rating using related posts
             average_rating = Post.objects.filter(restaurant_id=restaurant.id).aggregate(Avg('rating'))['rating__avg']
-            average_rating = round(average_rating, 1) if average_rating else None
+            average_rating = round(average_rating, 1) if average_rating is not None else 0
             # Update the average_rating field in the restaurant model
             restaurant.average_rating = average_rating
-            print("Average rating for is", restaurant, average_rating)
+            print("Average rating for", restaurant, "is", average_rating)
             restaurant.save()
 
         self.stdout.write(self.style.SUCCESS('Average ratings calculated successfully.'))
